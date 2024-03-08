66aa2567a3302eccb910c3abb3638a0b7a3bb943
Elodie Favier <elodie.favier@mailbox.org>
2024-03-08 10:18:32 +0100

    pb de liens dans les liens et de key ok

--- ArgentBank/src/components/layout/LoginLogout.jsx
+++ ArgentBank/src/components/layout/LoginLogout.jsx
@@ -1,6 +1,6 @@
 
 import '../../styles/main.css';
-import { Link } from 'react-router-dom';
+
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faCircleUser} from '@fortawesome/free-solid-svg-icons'
 import {faRightFromBracket} from '@fortawesome/free-solid-svg-icons'
@@ -8,11 +8,11 @@ import {faRightFromBracket} from '@fortawesome/free-solid-svg-icons'
 const LoginLogout = () => {
     return (
         <div className='main-nav-item'>
-            <Link className='main-nav-item' to='/signIn'>
+           
             <FontAwesomeIcon className='main-nav-item' icon={faCircleUser} />
             <FontAwesomeIcon className='icon-logout main-nav-item' icon={faRightFromBracket}/>
                 Sign In
-            </Link>
+          
         </div>
     );
 };
--- ArgentBank/src/components/layout/Logo.jsx
+++ ArgentBank/src/components/layout/Logo.jsx
@@ -1,12 +1,11 @@
 
-import { Link } from 'react-router-dom';
 import '../../styles/main.css';
 
 const Logo = () => {
     return (
        
-            <Link className="main-nav-logo" to='/'><img className="main-nav-logo-image" src='./argentBankLogo.png' alt='Logo de la banque'/> 
-            </Link>
+          <img className="main-nav-logo-image" src='./argentBankLogo.png' alt='Logo de la banque'/> 
+          
         
     );
 };
